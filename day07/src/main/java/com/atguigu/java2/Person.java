66bb0c42d2f60c7b6cb5ddf6a08f81a975bddea4
LiPeng-Admin <18654119633@163.com>
2022-03-05 11:12:51 +0800

    update3

--- day07/src/main/java/com/atguigu/java2/Person.java
+++ day07/src/main/java/com/atguigu/java2/Person.java
@@ -1,19 +1,51 @@
 package com.atguigu.java2;
 
 public class Person {
+
+    //属性
     String name;
     int age;
-    public void setName(String n){
-        name=n;
+    public void setName(String name){
+        this.name=name; //不能省略this,原因是形参name 与属性的name 同名
 
     }
-    public String getName(){
-        return name;
+    //构造器
+    public Person(){
+        String info="执行40行代码";
+        System.out.println(info);
+
     }
-    public void setAge(int i){
-        age=i;
+    public Person(String name){
+        this();
+        this.name=name;
+    }
+    public Person(int age){
+        this();
+        this.age=age;
+
+    }
+    public Person(String name,int age){
+        this(age);
+        this.name=name;
+        this.age=age;
+
+    }
+
+    //方法
+    public String getName(){
+        return this.name; //可以省略this
+    }
+    public void setAge(int age){
+        this.age=age; //不能省略this
     }
     public int getAge(){
-        return age;
+        return this.age; //可以省略this
+    }
+    public void eat(){
+        System.out.println("人可以吃饭");
+        this.study(); //可以省略this
+    }
+    public void study(){
+        System.out.println("人可以学习");
     }
 }
